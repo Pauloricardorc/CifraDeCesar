e7bc71b66cc5227c12f206d7ce6d07f2f5947589
Pauloricardorc <38074396+Pauloricardorc@users.noreply.github.com>
2018-11-19 07:24:55 -0800

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,33 +1,32 @@
-alfabeto = 'abcdefghijklmnopqrstuvwxyz'
-quantos = None
+alfabeto = 'abcdefghijklmnopqrstuvwxyz' #alfabeto completo atualizado
+quantos = None # tanto de casas a ser pulados no alfabeto
 
 def codificar(frase):
     m = ''
     for c in frase:
         if c in alfabeto:
-            c = alfabeto.index(c)
-            m += alfabeto[(c + quantos) % len(alfabeto)]
+            c = alfabeto.index(c) # pesquisando qual a posição da letra no alfabeto
+            m += alfabeto[(c + quantos) % len(alfabeto)] # ('fazer o alfabeto ficar em um loop para quando chega no 'z' ele voltar para o 'a')
         else:
-            m += c
-    print('Mensagem criptografada = {}'.format(m))
+            m += c #adicionar um espaço caso a letra da palavra digitada não esteja no alfabeto
+    print('Mensagem criptografada = {}'.format(m)) #resposta completa
 def descodificar(frase):
-    m = ''
+    m = '' 
     for c in frase:
-        if c in alfabeto:
-            c = alfabeto.index(c)
-            m += alfabeto[(c - quantos) % len(alfabeto)]
-        else:
-            m += c
+        if c in alfabeto: 
+            c = alfabeto.index(c) # pesquisando qual a posição da letra no alfabeto
+            m += alfabeto[(c - quantos) % len(alfabeto)]# ('fazer o alfabeto ficar em um loop para quando chega no 'z' ele voltar para o 'a')
+        else: # caso a letra não esteja no alfabeto
+            m += c #adicionar um espaço caso a letra da palavra digitada não esteja no alfabeto
     print('Mensagem descriptografada = {}'.format(m))
 print('______________________________________________________')
 frase  =  str(input('Escreva uma frase: '))
 pergunta1 = int(input('''Você deseija codificar ou descodificar? 
 1(codificar) ou 2(descodificar)? '''))
-quantos = int(input('Tanto de casas a ser puladas: '))
+quantos = int(input('Chave: ')) # casas a ser puladas no alfabeto
 print('______________________________________________________')
 
 if (pergunta1 == 1):
     codificar(frase)
 elif(pergunta1 == 2):
-    descodificar(frase)
-input('>')
+ Chave('>') # casas a ser puladas no alfabeto
